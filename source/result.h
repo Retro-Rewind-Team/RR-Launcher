db6bdf7c95d18d74113482b2af7f05b7ccd097d6
Jacher <jwc2002@outlook.com>
2025-04-12 23:55:13 +0100

    ceate result basics

---
+++ source/result.h
@@ -0,0 +1,127 @@
+/*
+    result.h - Error and success handling
+
+    Copyright (C) 2025  Retro Rewind Team
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef RRC_RESULT_H
+#define RRC_RESULT_H
+
+#include <gctypes.h>
+#include <errno.h>
+#include <string.h>
+#include <curl/curl.h>
+
+/* The amount of time in seconds to display fatal errors before exiting */
+#define RRC_RESULT_FATAL_SPLASH_TIME_SEC 20
+
+enum rrc_result_error_source
+{
+    /* Success condition */
+    ESOURCE_NONE = -1,
+    ESOURCE_CURL,
+    ESOURCE_ERRNO,
+    ESOURCE_ZIP
+};
+
+/* Because each library uses their own set of error codes, we need to support all
+   of them and be able to stringify all of them for display if they occur. */
+union rrc_result_error_inner
+{
+    /* defined if curl has an error */
+    CURLcode ccode;
+    /* defined for any libc errors */
+    int errnocode;
+    /* defined for ZIP errors */
+    int ziperr;
+};
+
+/* Primary result struct. Denotes either success or failure of a routine.
+   Success is considered a no-op in most cases, and errors are handled in different
+   ways depending on the context and severity.
+
+   Errors have two main escape routes:
+      - Fatal errors cannot be recovered from. The most serious of errors will
+        display themselves on-screen, and then a fixed-length delay will happen
+        before exiting. This is because we need to handle cases where the
+        controller is not initialized, as this prevents us from reading the
+        controller in order to, for example, support pressing A to exit etc.
+        Some examples of fatal errors include failure to initialise SD card,
+        failure to initialise DVD drive, can't init controllers, etc.
+
+      - Normal errors are errors that can be recovered from. When a normal error
+        occurs, the user is displayed an error prompt which they can dismiss.
+        Some examples include failure to download updates, failed to save settings
+        etc.
+        It is implementation defined exactly what happens when a normal error occurs.
+        See `rrc_result_error_check_error_normal' for details.
+
+    This result struct should rarely (or never) be interfaced with directly.
+    Instead, use the plethora of helper functions in order to process and handle
+    errors. For example, to check for error and react accordingly, call the
+    functions `rrc_result_check_error_fatal' or `rrc_error_check_error_normal',
+    depending on the context of the error condition. For example, early stage
+    SD card failure would use the former function. See their documentaton for
+    more details.
+
+    Result structs can be created with `rrc_result_create_*' (depending on source).
+    Result structures are designed to be stack-allocated (i.e., not using `malloc').
+*/
+struct rrc_result
+{
+    /* the relevant union member - set to -1 on success */
+    enum rrc_result_error_source errtype;
+
+    /* the following fields are only defined in the event of an error */
+    /* the actual source of the error */
+    union rrc_result_error_inner inner;
+    /* static string with any additional error context */
+    const char *context;
+};
+
+struct rrc_result rrc_result_create_success();
+
+struct rrc_result rrc_result_create_error_curl(CURLcode error, const char *context);
+
+struct rrc_result rrc_result_create_error_errno(int eno, const char *context);
+
+struct rrc_result rrc_result_create_error_zip(int error, const char *context);
+
+/* Returns true if this result is an error, false otherwise. */
+bool rrc_result_is_error(struct rrc_result *result);
+
+/* Returns a statically allocated string with contextual information related to
+   the inner error code. NULL if unknown or not an error. The returned string
+   should not be freed. */
+char *rrc_result_strerror(struct rrc_result *result);
+
+/* Check this result for error condition, and if it is in an erroneous state, supply
+   a prompt with the error details. This prompt can be dismissed.
+
+   It is down to the particular implementation at which this error happens to decide
+   how exactly to handle the error condition. It is likely that this is propagated
+   back to the call site of thie work (such as the settings page) and no other
+   action is taken. */
+void rrc_result_error_check_error_normal(struct rrc_result *result);
+
+/* Check this result for error condition, and if it is in an erroneous state, display
+   an error message, wait a set period of time, and exit.
+
+   Unlike normal errors, this can be called from anywhere because either it does
+   nothing (success) or never returns (error). */
+void rrc_result_error_check_error_fatal(struct rrc_result *result);
+
+#endif
